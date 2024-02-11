a7b79845269533d898a4d38b423c7b595097f17c
Tejas013 <tejasmadhukar6@gmail.com>
2024-02-11 12:50:25 +0530

    added word_embedding route

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from opensearchpy import OpenSearch
 from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
+from models import Query
+import uvicorn
+import torch
+import boto3
 import os
 
+BIN_DIR = "bin/"
+
 load_dotenv()
+s3 = boto3.client("s3")
+model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
 app = FastAPI()
 client = OpenSearch(
     hosts=[{"host": os.getenv("OPENSEARCH_HOST", "0.0.0.0"), "port": 9200}],
@@ -16,7 +25,34 @@ client = OpenSearch(
     ssl_show_warn=False,
 )
 
+if os.path.isdir(BIN_DIR):
+    os.mkdir("BIN_DIR")
+    bucket_content = s3.list_objects(Bucket="sentence-index")
+    for obj in bucket_content["Contents"]:
+        file_name = os.path.join(BIN_DIR, obj["key"])
+        s3.download_file("sentence-index", obj["key"], file_name)
+
 
 @app.get("/health")
 async def health():
     return {"message": "Healthy"}
+
+
+@app.get("/word_similarity")
+async def word_similarity(request: Query):
+    with torch.no_grad():
+        mean_pooled = model.encode(request.message)
+
+    query = {
+        "size": 5,
+        "query": {"knn": {"embedding": {"vector": mean_pooled, "k": 2}}},
+        "_source": False,
+        "fields": ["Case Number", "Case Title", "Judgement Date", "Judgement PDF URL"],
+    }
+
+    response = client.search(body=query, index="word_embedding")
+    return response["hits"]["hits"]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
