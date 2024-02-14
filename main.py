63f717614384110cae875159a1b4340d1f898fe3
Tejas013 <tejasmadhukar6@gmail.com>
2024-02-14 12:39:03 +0530

    updated handlers

--- main.py
+++ main.py
@@ -5,15 +5,9 @@ from sentence_transformers import SentenceTransformer
 from models import Query
 import uvicorn
 import torch
-import boto3
-import pickle
-import json
 import os
 
-BIN_DIR = "bin/"
-
 load_dotenv()
-s3 = boto3.client("s3")
 model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
 app = FastAPI()
 client = OpenSearch(
@@ -27,32 +21,14 @@ client = OpenSearch(
     ssl_show_warn=False,
 )
 
-if not os.path.isdir(BIN_DIR):
-    os.mkdir(BIN_DIR)
-    bucket_content = s3.list_objects(Bucket="sentence-index")
-    for obj in bucket_content["Contents"]:
-        object_name = obj["Key"]
-        file_name = os.path.join(BIN_DIR, object_name)
-        s3.download_file("sentence-index", object_name, file_name)
 
-
-with open(BIN_DIR+"index.pkl", "rb") as file_handle:
-    index = pickle.load(file_handle)
-
-with open(BIN_DIR+"case_uid_to_case_info.json", "rb") as file_handle:
-    case_uid_to_case_info = json.load(file_handle)
-
-with open(BIN_DIR+"uid_to_sentence_mapping.json", "rb") as file_handle:
-    uid_to_sentence_mapping = json.load(file_handle)
-
-
-@app.get("/health")
+@app.get("/")
 async def health():
     return {"message": "Healthy"}
 
 
-@app.post("/word_similarity")
-async def word_similarity(request: Query):
+@app.post("/semantic_similarity")
+async def semantic_similarity(request: Query):
     with torch.no_grad():
         mean_pooled = model.encode(request.message)
 
@@ -69,25 +45,24 @@ async def word_similarity(request: Query):
 
 @app.post("/sentence_similarity")
 async def sentence_similarity(request: Query):
-    query_embedding = model.encode([request.message])
-    response = []
-    _ , uid = index.search(n=1, x=query_embedding, k=3)
+    with torch.no_grad():
+        mean_pooled = model.encode(request.message)
 
-    for n, i in enumerate(uid[0]):
-        mini_response = {}
-        case_no = str(i)[-5:]
-        case_sent = str(i)[:-5]
-        case_info = case_uid_to_case_info[str(int(case_no))]
-        mini_response['ResultNumber'] = n+1
-        mini_response['CaseNumber'] = case_info["c_no"]
-        mini_response["CaseTitle"] = case_info["c_t"]
-        mini_response["JudgementDate"] = case_info["j_d"]
-        mini_response["PdfUrl"] = case_info["pdf"]
-        mini_response["CaseText"] = case_sent
-        mini_response["SimilarSentence"] = uid_to_sentence_mapping[str(i)]
-        response.append(mini_response)
+    query = {
+        "size": 5,
+        "query": {"knn": {"embedding": {"vector": mean_pooled, "k": 3}}},
+        "_source": False,
+        "fields": [
+            "Judgement PDF URL",
+            "Case Number",
+            "Case Title",
+            "Judgement Date",
+            "Sentence",
+        ],
+    }
 
-    return response
+    response = client.search(body=query, index="sentence")
+    return response["hits"]["hits"]
 
 
 if __name__ == "__main__":
